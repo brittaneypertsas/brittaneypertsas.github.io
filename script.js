47745edb302fd1f2766fc1217848413272419391
brittaneypertsas <35590139+brittaneypertsas@users.noreply.github.com>
2018-05-11 06:35:05 -0400

    fixed jquery

--- script.js
+++ script.js
@@ -8,14 +8,13 @@ $("#hidestuff").hover(function(){
   $("ul").hide();
 });
 
+$("button").click(function(){
+
  $("#fadein1").fadeIn(1000);
-});
 
  $("#fadein2").fadeIn(2000);
-});
 
  $("#fadein3").fadeIn(3000);
-});
 
  $("fadein4").fadeIn(4000);
 });
