b5eb5ffec623657ae74def6385042267997615fa
brittaneypertsas <35590139+brittaneypertsas@users.noreply.github.com>
2018-05-11 08:00:16 -0400

    jqueryupdates

--- script.js
+++ script.js
@@ -19,5 +19,11 @@ $("button").click(function(){
  $("fadein4").fadeIn(4000);
 });
 
+$(".togglebox").toggle(8000);
+
+$("#target").on("click", function() {
+  alert("Thanks for visiting! Hope to see you again in the French Horn world :)");
+});
+
 
 });
