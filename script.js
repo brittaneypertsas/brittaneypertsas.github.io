19975a03e109967e673cabe83f54933663d0bfa1
brittaneypertsas <35590139+brittaneypertsas@users.noreply.github.com>
2018-05-11 01:32:39 -0400

    deleted jquery

--- script.js
+++ script.js
@@ -1,30 +1,5 @@
 $(document).ready(function(){
-    $("button").click(function(){
-
-        $("#box2").fadeIn("3000");
-        $("#box3").fadeIn("5000");
-
-    });
-    $("button").click(function(){
-
-        $("#box2").fadeOut("3000");
-        $("#box3").fadeOut("5000");
-
-});
-
-    $(".showstuff").hover(function(){
-      $("ul").show();
-  });
-
-  $(".hidestuff").hover(function(){
-    $("ul").hide();
-});
-
-$("#togglebox").toggle(8000);
-
-$("#target").on("click", function() {
-  alert("Enjoy your food!");
-});
+    
 
 
 
